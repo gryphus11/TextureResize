cfb3a53fe9bdd99e32dc0306bde2f3c98fb4f38d
Brunhild <nechwi1121@gmail.com>
2018-08-28 10:11:45 +0900

    기존의 리사이징 프로젝트 불러옴

---
+++ TextureResize/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using TextureResize.Sources;
+using System.Threading;
+
+namespace TextureResize
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int width = 0;
+            int height = 0;
+            string[] arguments = null;
+
+            InputArguments(args, out arguments, out width, out height);
+
+            Resize(arguments, width, height);
+
+            Console.WriteLine("모든 텍스쳐의 변환 완료! 엔터 키를 눌러서 종료.\n");
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+        }
+
+        private static void InputArguments(string[] args, out string[] outArguments, out int outWidth, out int outHeight)
+        {
+            int width = 0;
+            int height = 0;
+            string[] arguments = null;
+
+            if (args.Length == 0)
+            {
+                Console.Write("파일의 경로와 파일명까지 입력해 주세요. ',' 로 구분하여 경로 추가: ");
+
+                string inputPath = Console.ReadLine();
+                arguments = inputPath.Trim().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string argument in arguments)
+                {
+                    Console.WriteLine(argument);
+                }
+            }
+            else
+            {
+                arguments = args;
+            }
+
+            bool validValue = false;
+
+            do
+            {
+                Console.Write("리사이징 텍스쳐의 Width를 입력해 주세요 : ");
+                string widthString = Console.ReadLine();
+                if (int.TryParse(widthString, out width))
+                {
+                    validValue = true;
+                }
+                else
+                {
+                    Console.WriteLine("올바른 입력값이 아닙니다.\n");
+                }
+            } while (!validValue);
+
+            validValue = false;
+
+            do
+            {
+                Console.Write("리사이징 텍스쳐의 Height를 입력해 주세요 : ");
+                string heightString = Console.ReadLine();
+                if (int.TryParse(heightString, out height))
+                {
+                    validValue = true;
+                }
+                else
+                {
+                    Console.WriteLine("올바른 입력값이 아닙니다.\n");
+                }
+            } while (!validValue);
+
+            outArguments = arguments;
+            outWidth = width;
+            outHeight = height;
+        }
+
+        private static void Resize(string[] arguments, int width, int height)
+        {
+            List<TextureLoader> loaders = new List<TextureLoader>();
+            for (int i = 0; i < arguments.Length; ++i)
+            {
+                TextureLoader loader = new TextureLoader();
+                loader.ResizeTexture(arguments[i], width, height);
+                loaders.Add(loader);
+            }
+
+            bool isDone = false;
+            while (!isDone)
+            {
+                isDone = true;
+                foreach (TextureLoader loader in loaders)
+                {
+                    if (!loader.isResizingDone)
+                    {
+                        isDone = false;
+                        break;
+                    }
+                }
+            }
+
+            loaders.Clear();
+            loaders = null;
+        }
+    }
+}
---
+++ TextureResize/Sources/TextureLoader.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace TextureResize.Sources
+{
+    public class TextureLoader
+    {
+        public bool isResizingDone = false;
+
+        private Bitmap LoadImage(string path)
+        {
+            Bitmap texture = null;
+            if (File.Exists(path))
+            {
+                FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                texture = new Bitmap(stream);
+                stream.Close();
+
+                Console.WriteLine(path + " 읽기 완료! " + texture.Size + "\n");
+
+                return texture;
+            }
+            return null;
+        }
+
+        private void ResizeImage2(string path, int width, int height)
+        {
+            if (File.Exists(path))
+            {
+                string directory = Path.GetDirectoryName(path);
+                string fileName = Path.GetFileName(path);
+
+                FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                Image sourceImage = Image.FromStream(stream);
+                Bitmap sourceTexture = new Bitmap(sourceImage, new Size(width, height));
+                stream.Close();
+                sourceTexture.Save(directory + "/new2_" + fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                sourceTexture.Dispose();
+            }
+            else
+            {
+                Console.WriteLine(path + " => 원본 텍스쳐를 찾을수 없습니다.\n");
+            }
+        }
+
+        private void ResizeImage(string path, int width, int height)
+        {
+            Bitmap sourceTexture = LoadImage(path);
+            string fileName = string.Empty;
+            string directory = string.Empty;
+
+            int targetWidth = width;
+            int targetHeight = height;
+
+            if (sourceTexture == null)
+            {
+                Console.WriteLine(path + " => 원본 텍스쳐를 찾을수 없습니다.\n");
+                return;
+            }
+
+            directory = Path.GetDirectoryName(path);
+            fileName = Path.GetFileName(path);
+
+            int sourceWidth = sourceTexture.Width;
+            int sourceHeight = sourceTexture.Height;
+
+            if (targetHeight > sourceHeight)
+            {
+                targetHeight = sourceHeight;
+            }
+
+            if (targetWidth > sourceWidth)
+            {
+                targetWidth = sourceWidth;
+            }
+
+            if ((sourceWidth <= targetWidth) && (sourceHeight <= targetHeight))
+            {
+                Console.WriteLine(fileName + " 은 리사이징이 필요하지 않습니다.\n");
+                return;
+            }
+
+            int totalTargetLength = targetWidth * targetHeight;
+
+            int widthPixelRate = (sourceWidth / targetWidth);
+            int heightPixelRate = (sourceHeight / targetHeight);
+
+            int colorBlendedCount = 0;
+
+            Bitmap newBitmap = new Bitmap(targetWidth, targetHeight);
+            Console.WriteLine(fileName + " 의 색상 정보의 평균을 처리중\n");
+
+            int maxIndex = totalTargetLength - 1;
+            float targetRate = 10.0f;
+
+            for (int totalIndex = 0; totalIndex < totalTargetLength; ++totalIndex)
+            {
+                int startY = (totalIndex / targetWidth) * heightPixelRate;
+                int startX = (totalIndex % targetWidth) * widthPixelRate;
+                int endY = startY + heightPixelRate;
+                int endX = startX + widthPixelRate;
+
+                int alpha = 0;
+                int red = 0;
+                int green = 0;
+                int blue = 0;
+
+                for (int y = startY; y < endY; ++y)
+                {
+                    for (int x = startX; x < endX; ++x)
+                    {
+                        Color sourcePixel = sourceTexture.GetPixel(x, y);
+
+                        alpha += sourcePixel.A;
+                        red += sourcePixel.R;
+                        green += sourcePixel.G;
+                        blue += sourcePixel.B;
+
+                        ++colorBlendedCount;
+                    }
+                }
+
+                int avgAlpha = alpha / colorBlendedCount;
+                int avgRed = red / colorBlendedCount;
+                int avgGreen = green / colorBlendedCount;
+                int avgBlue = blue / colorBlendedCount;
+
+                Color averageColor = Color.FromArgb(avgAlpha, avgRed, avgGreen, avgBlue);
+
+                colorBlendedCount = 0;
+
+                newBitmap.SetPixel(totalIndex % targetWidth, totalIndex / targetWidth, averageColor);
+
+
+                float percentage = ((float)totalIndex / maxIndex * 100.0f);
+                if (percentage >= targetRate)
+                {
+                    targetRate += 10.0f;
+                    Console.WriteLine(fileName + " " + percentage + "% 완료\n");
+                }
+            }
+            Console.WriteLine("리사이징 완료된 " + fileName + " 의 저장 시작\n");
+            newBitmap.Save(directory + "/new_" + fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            newBitmap.Dispose();
+            Console.WriteLine("저장 완료 : " + directory + "/new_" + fileName + "\n");
+        }
+
+        async public void ResizeTexture(string path, int width, int height)
+        {
+            Task task = Task.Run(() => { ResizeImage2(path, width, height); });
+            Console.WriteLine(path + " [리사이징 시작]\n");
+            await task;
+            Console.WriteLine(path + " [리사이징 완료]\n");
+            isResizingDone = true;
+        }
+
+    }
+}
